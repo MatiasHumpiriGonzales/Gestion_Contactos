1e20dad31959c53ac833f04991c0f90a48ff11d6
Matias <mhumpirig@unjbg.edu.pe>
2025-06-28 21:29:56 -0500

    Agregando Ejercicio1.cpp con struct contactoEmail

---
+++ Ejercicio1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct contactoEmail {
+	string nombreCompleto;
+	char sexo;
+	int edad;
+	string telefono;
+	string email;
+	string nacionalidad;
+};
