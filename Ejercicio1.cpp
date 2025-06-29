281cda2d1b69913d6749c83685c41da98d02005a
Matias <mhumpirig@unjbg.edu.pe>
2025-06-28 21:35:01 -0500

    Agregando menú indicando las opciones que se pueden hacer

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -9,3 +9,19 @@ struct contactoEmail {
 	string email;
 	string nacionalidad;
 };
+int main (){
+	arreglo[contactoEmail] > contactos;
+	int opcion;
+	
+	do {
+		cout << "\n----- MENU -----\n";
+        cout << "1. Agregar contacto\n";
+        cout << "2. Eliminar contacto\n";
+        cout << "3. Mostrar listado general\n";
+        cout << "4. Mostrar contactos ordenados por servidor de correo\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+        cin.ignore();"
+	}
+}
