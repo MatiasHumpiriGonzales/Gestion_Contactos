ce6def4d29c9db8263a050245e2e84f398b1a8e2
Matias <mhumpirig@unjbg.edu.pe>
2025-06-28 22:03:38 -0500

    Se agrego un contador de contactos maximo de 100

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
+const int MAX_CONTACTOS = 100;
 struct contactoEmail {
-	string nombreCompleto;
-	char sexo;
-	int edad;
-	string telefono;
-	string email;
-	string nacionalidad;
+    string nombreCompleto;
+    char sexo;
+    int edad;
+    string telefono;
+    string email;
+    string nacionalidad;
 };
-int main (){
-	arreglo[contactoEmail] > contactos;
-	int opcion;
-	
-	do {
-		cout << "\n----- MENU -----\n";
+int main () {
+    contactoEmail est[MAX_CONTACTOS];
+    int total_contactos = 0;
+    int opcion;
+    do {
+        cout << "\n----- MENU -----\n";
         cout << "1. Agregar contacto\n";
         cout << "2. Eliminar contacto\n";
         cout << "3. Mostrar listado general\n";
@@ -22,6 +23,6 @@ int main (){
         cout << "5. Salir\n";
         cout << "Seleccione una opcion: ";
         cin >> opcion;
-        cin.ignore();"
-	}
+        cin.ignore();
 }
+ 
